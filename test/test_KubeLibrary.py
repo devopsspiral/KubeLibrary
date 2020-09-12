f59f0a8049dfdecfb9c329259711177337a62539
M Wcislo <devopsspiral@gmail.com>
2020-09-12 09:39:43 +0200

    example kubeconfig

--- test/test_KubeLibrary.py
+++ test/test_KubeLibrary.py
@@ -48,38 +48,38 @@ def mock_list_namespaced_pod(namespace, watch=False, label_selector=""):
 class TestKubeLibrary(unittest.TestCase):
 
     def test_KubeLibrary_inits_from_kubeconfig(self):
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
 
     def test_filter_pods_names(self):
         pods_items = mock_list_namespaced_pod('default')
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         self.assertEqual(['octopus-0', 'grafana-5d9895c6c4-sfsn8'], kl.filter_pods_names(pods_items.items))
 
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_get_all_pods_in_namespace(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('.*', 'default')
         self.assertEqual(['octopus-0', 'grafana-5d9895c6c4-sfsn8'], kl.filter_pods_names(pods))
 
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_get_matching_pods_in_namespace(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('graf.*', 'default')
         self.assertEqual(['grafana-5d9895c6c4-sfsn8'], kl.filter_pods_names(pods))
 
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_filter_pods_containers_by_name(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('octopus.*', 'default')
         self.assertEqual('manager', kl.filter_pods_containers_by_name(pods, '.*')[0].name)
 
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_filter_containers_images(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('octopus.*', 'default')
         containers = kl.filter_pods_containers_by_name(pods, '.*')
         self.assertEqual(['eu.gcr.io/kyma-project/incubator/develop/octopus:dc5dc284'], kl.filter_containers_images(containers))
@@ -87,21 +87,21 @@ class TestKubeLibrary(unittest.TestCase):
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_filter_pods_containers_statuses_by_name(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('octopus.*', 'default')
         self.assertEqual(0, kl.filter_pods_containers_statuses_by_name(pods, '.*')[0].restart_count)
 
     @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
     def test_filter_containers_resources(self, mock_lnp):
         mock_lnp.side_effect = mock_list_namespaced_pod
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         pods = kl.get_pods_in_namespace('octopus.*', 'default')
         containers = kl.filter_pods_containers_by_name(pods, '.*')
         self.assertEqual('100m', kl.filter_containers_resources(containers)[0].limits.cpu)
 
     def test_assert_pod_has_labels(self):
         pod = mock_list_namespaced_pod('default').items[0]
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         labels = '{}'
         self.assertTrue(kl.assert_pod_has_labels(pod, labels))
         labels = '{"app":"octopus"}'
@@ -115,7 +115,7 @@ class TestKubeLibrary(unittest.TestCase):
 
     def test_assert_pod_has_annotations(self):
         pod = mock_list_namespaced_pod('default').items[1]
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         labels = '{}'
         self.assertTrue(kl.assert_pod_has_annotations(pod, labels))
         labels = '{"checksum/config":"1c42968a1b9eca0bafc3273ca39c4705fe71dc632e721db9e8ce44ab1b8e1428"}'
@@ -129,7 +129,7 @@ class TestKubeLibrary(unittest.TestCase):
 
     def test_assert_container_has_env_vars(self):
         pod = mock_list_namespaced_pod('default').items[0]
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         container = kl.filter_pods_containers_by_name([pod], '.*')[0]
         env_vars = '{}'
         self.assertTrue(kl.assert_container_has_env_vars(container, env_vars))
@@ -144,7 +144,7 @@ class TestKubeLibrary(unittest.TestCase):
 
     @unittest.skip("Will overwrite test/resources/pods.json")
     def test_gather_pods_obejcts_to_pods_json(self):
-        kl = KubeLibrary(kube_config='~/.kube/k3d')
+        kl = KubeLibrary(kube_config='test/resources/k3d')
         ret = kl.v1.list_namespaced_pod('default', watch=False)
         pods_str = str(ret.items).replace("'", '"') \
                                  .replace('None', 'null') \
