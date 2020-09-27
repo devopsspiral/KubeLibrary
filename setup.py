514f54cefa93addea8159cf0f9325db69f8ba759
M Wcislo <devopsspiral@gmail.com>
2020-09-27 11:17:49 +0200

    back to final

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robotframework-kubelibrary",
-    version="0.0.2",
+    version="0.2.0",
     author="Michał Wcisło",
     author_email="mwcislo999@gmail.com",
     description="Kubernetes library for Robot Framework",
