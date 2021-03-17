e276e56db6b2c54f70127beb2f62165aeca0c37a
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-03-17 10:08:54 +0530

    Update test_KubeLibrary.py
    Added 'rbac_authv1_api' into apis.

--- test/test_KubeLibrary.py
+++ test/test_KubeLibrary.py
@@ -103,7 +103,7 @@ def mock_list_node_info(watch=False, label_selector=""):
 class TestKubeLibrary(unittest.TestCase):
 
     apis = ('v1', 'extensionsv1beta1', 'batchv1', 'appsv1', 'batchv1_beta1',
-            'custom_object')
+            'custom_object', 'rbac_authv1_api')
 
     def test_KubeLibrary_inits_from_kubeconfig(self):
         KubeLibrary(kube_config='test/resources/k3d')
