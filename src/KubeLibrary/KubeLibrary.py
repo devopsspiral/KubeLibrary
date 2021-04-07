b87d644d9d650c91ee4328a719e2702186371c82
satish-nubolab <78336217+satish-nubolab@users.noreply.github.com>
2021-04-07 19:27:13 +0530

    Update KubeLibrary.py

--- src/KubeLibrary/KubeLibrary.py
+++ src/KubeLibrary/KubeLibrary.py
@@ -756,7 +756,7 @@ class KubeLibrary(object):
           Namespace to check
         """
         ret = self.batchv1_beta1.create_namespaced_cron_job(namespace=namespace, body=body)
-        return ret	
+        return ret
 
     def delete_cron_job_in_namespace(self, name, namespace):
         """Deletes cron_job in a namespace
