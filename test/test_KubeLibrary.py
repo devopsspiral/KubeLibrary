650ec8c0806cf316b94613a1889dbcec100d1caa
Raymond Zheng <raymond.zheng@health.telstra.com>
2022-06-23 23:04:24 +1000

    debug

--- test/test_KubeLibrary.py
+++ test/test_KubeLibrary.py
@@ -786,4 +786,4 @@ class TestKubeLibrary(unittest.TestCase):
 
     @mock.patch('kubernetes.client.ApiextensionsV1Api.read_cluster_custom_definition')
     def test_read_cluster_custom_definition(self, mock_stream):
-        pass
+        kl = KubeLibrary(kube_config='test/resources/k3d')
